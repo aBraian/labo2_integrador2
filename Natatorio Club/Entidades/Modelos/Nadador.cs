e4a50818a337c4b7b6e1a44a5045e63cf4e5ccef
aBraian <abraian56@gmail.com>
2023-11-22 17:53:13 -0300

    Commit Nro 12 - Documentacion Modelos

--- Natatorio Club/Entidades/Modelos/Nadador.cs
+++ Natatorio Club/Entidades/Modelos/Nadador.cs
@@ -19,12 +19,25 @@ namespace Entidades.Modelos
         private static int edadMinima;
         private static DateTime minimoFechaNacimiento;
 
+        /// <summary>
+        /// Inicializa la clase Nadador con la edad mínima requerida y la fecha de nacimiento mínima permitida.
+        /// </summary>
         static Nadador()
         {
             edadMinima = 4;
             minimoFechaNacimiento = new DateTime(1950, 1, 1);
         }
 
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase Nadador con los datos proporcionados.
+        /// </summary>
+        /// <param name="nombre">Nombre del nadador.</param>
+        /// <param name="apellido">Apellido del nadador.</param>
+        /// <param name="dni">DNI del nadador.</param>
+        /// <param name="telefono">Número de teléfono del nadador.</param>
+        /// <param name="fechaNacimiento">Fecha de nacimiento del nadador.</param>
+        /// <param name="turno">Turno en el que se inscribe el nadador.</param>
+        /// <param name="nivel">Nivel de experiencia del nadador.</param>
         public Nadador(string nombre, string apellido, string dni, string telefono, DateTime fechaNacimiento, 
             ETurno turno, ENivel nivel)
         {
@@ -38,6 +51,9 @@ namespace Entidades.Modelos
             this.nivel = nivel;
         }
 
+        /// <summary>
+        /// Obtiene o establece el nombre de la persona.
+        /// </summary>
         public string Nombre
         {
             get => nombre;
@@ -57,6 +73,9 @@ namespace Entidades.Modelos
             }
         }
 
+        /// <summary>
+        /// Obtiene o establece el apellido de la persona.
+        /// </summary>
         public string Apellido
         {
             get => apellido;
@@ -76,6 +95,9 @@ namespace Entidades.Modelos
             }
         }
 
+        /// <summary>
+        /// Obtiene o establece el número de DNI de la persona.
+        /// </summary>
         public string Dni
         {
             get => dni;
@@ -95,6 +117,9 @@ namespace Entidades.Modelos
             }
         }
 
+        /// <summary>
+        /// Obtiene o establece el número de teléfono de la persona.
+        /// </summary>
         public string Telefono
         {
             get => telefono;
@@ -118,6 +143,9 @@ namespace Entidades.Modelos
             }
         }
 
+        /// <summary>
+        /// Obtiene o establece la fecha de nacimiento de la persona.
+        /// </summary>
         public DateTime FechaNacimiento
         {
             get => fechaNacimiento;
@@ -136,10 +164,20 @@ namespace Entidades.Modelos
             }
         }
 
+        /// <summary>
+        /// Obtiene o establece el turno en el que se inscribe la persona.
+        /// </summary>
         public ETurno Turno { get => turno; set => turno = value; }
 
+        /// <summary>
+        /// Obtiene o establece el nivel de experiencia de la persona.
+        /// </summary>
         public ENivel Nivel { get => nivel; set => nivel = value; }
 
+        /// <summary>
+        /// Obtiene o establece la edad del nadador. 
+        /// Se ignora en la serialización JSON.
+        /// </summary>
         [System.Text.Json.Serialization.JsonIgnore]
         public int Edad
         {
@@ -158,18 +196,35 @@ namespace Entidades.Modelos
             }
         }
 
+        /// <summary>
+        /// Obtiene el deporte al que pertenece el nadador.
+        /// Se ignora en la serialización JSON.
+        /// </summary>
         [System.Text.Json.Serialization.JsonIgnore]
         public EDeporte Deporte { get => EDeporte.Natacion; }
 
+        /// <summary>
+        /// Obtiene la información del nadador. 
+        /// Se ignora en la serialización JSON.
+        /// </summary>
         [System.Text.Json.Serialization.JsonIgnore]
         public string Informacion { get => ObtenerInformacion(); }
 
+        /// <summary>
+        /// Obtiene una copia del nadador.
+        /// Se ignora en la serialización JSON.
+        /// </summary>
         [System.Text.Json.Serialization.JsonIgnore]
         public Nadador Copia
         {
             get => new Nadador(nombre, apellido, dni, telefono, fechaNacimiento, turno, nivel);
         }
 
+        /// <summary>
+        /// Formatea el DNI con puntos cada tres dígitos.
+        /// </summary>
+        /// <param name="dni">Número de DNI a formatear.</param>
+        /// <returns>Número de DNI formateado.</returns>
         private string DarFormatoDni(string dni)
         {
             int contador = 0;
@@ -186,6 +241,12 @@ namespace Entidades.Modelos
             return dniFormateado;
         }
 
+        /// <summary>
+        /// Valida el formato del número de teléfono y verifica el prefijo.
+        /// </summary>
+        /// <param name="telefono">Número de teléfono a validar.</param>
+        /// <returns>True si el formato y prefijo son válidos; de lo contrario, false.</returns>
+        /// <exception cref="PrefijoInvalidoException">Lanzada si el prefijo es inválido.</exception>
         private bool ValidarTelefono(string telefono)
         {
             if (telefono[0] != '1' || (telefono[1] != '1' && telefono[1] != '5'))
@@ -195,6 +256,11 @@ namespace Entidades.Modelos
             return telefono.ValidarEnteroPositivo(10);
         }
 
+        /// <summary>
+        /// Obtiene la información completa del nadador incluyendo nombre, apellido, DNI, teléfono, 
+        /// fecha de nacimiento, edad, turno, deporte y nivel.
+        /// </summary>
+        /// <returns>Información completa del nadador.</returns>
         private string ObtenerInformacion()
         {
             StringBuilder informacion = new StringBuilder();
@@ -227,11 +293,23 @@ namespace Entidades.Modelos
             return HashCode.Combine(dni, turno);
         }
 
+        /// <summary>
+        /// Compara dos nadadores para determinar si son iguales, basándose en el DNI y el turno.
+        /// </summary>
+        /// <param name="x">Primer nadador a comparar.</param>
+        /// <param name="y">Segundo nadador a comparar.</param>
+        /// <returns>True si los nadadores son iguales; de lo contrario, false.</returns>
         public static bool operator ==(Nadador x, Nadador y)
         {
             return x.dni == y.dni && x.turno == y.turno;
         }
 
+        /// <summary>
+        /// Compara dos nadadores para determinar si son diferentes, basándose en el DNI y el turno.
+        /// </summary>
+        /// <param name="x">Primer nadador a comparar.</param>
+        /// <param name="y">Segundo nadador a comparar.</param>
+        /// <returns>True si los nadadores son diferentes; de lo contrario, false.</returns>
         public static bool operator !=(Nadador x, Nadador y)
         {
             return !(x == y);
--- Natatorio Club/Entidades/Modelos/Natatorio.cs
+++ Natatorio Club/Entidades/Modelos/Natatorio.cs
@@ -7,6 +7,10 @@ namespace Entidades.Modelos
         private List<Nadador> listaNadadores;
         private int capacidad;
 
+        /// <summary>
+        /// Constructor privado de la clase Natatorio.
+        /// Inicializa la lista de nadadores si es nula.
+        /// </summary>
         private Natatorio()
         {
             if (listaNadadores is null) 
@@ -15,11 +19,22 @@ namespace Entidades.Modelos
             }
         }
 
+        /// <summary>
+        /// Constructor de la clase Natatorio.
+        /// Inicializa un natatorio con una capacidad especificada.
+        /// </summary>
+        /// <param name="capacidad">Capacidad máxima del natatorio.</param>
         public Natatorio(int capacidad) : this()
         {
             this.capacidad = capacidad;
         }
 
+        /// <summary>
+        /// Obtiene el índice de un nadador en la lista.
+        /// </summary>
+        /// <param name="nadador">Nadador a buscar en la lista.</param>
+        /// <returns>Índice del nadador en la lista.</returns>
+        /// <exception cref="NatatorioException">Se lanza cuando el nadador no se encuentra en la lista.</exception>
         public int this[Nadador nadador]
         {
             get
@@ -33,6 +48,10 @@ namespace Entidades.Modelos
             }
         }
 
+        /// <summary>
+        /// Propiedad de la clase Natatorio.
+        /// Obtiene y establece la lista de nadadores.
+        /// </summary>
         public List<Nadador> ListaNadadores
         {
             get => listaNadadores;
@@ -45,6 +64,9 @@ namespace Entidades.Modelos
             }
         }
 
+        /// <summary>
+        /// Ordena la lista de nadadores según el apellido, nombre, DNI y turno.
+        /// </summary>
         public void Ordenar()
         {
             listaNadadores.Sort((x, y) =>
@@ -68,12 +90,22 @@ namespace Entidades.Modelos
             });
         }
 
+        /// <summary>
+        /// Reemplaza un nadador existente en el natatorio con otro nadador.
+        /// </summary>
+        /// <param name="nadador">Nadador a ser reemplazado.</param>
+        /// <param name="reemplazo">Nuevo nadador que reemplazará al anterior.</param>
         public void Reemplazar(Nadador nadador, Nadador reemplazo)
         {
             int indice = this[nadador];
             listaNadadores[indice] = reemplazo;
         }
 
+        /// <summary>
+        /// Cuenta la cantidad de nadadores en el natatorio que tienen el mismo turno que el nadador especificado.
+        /// </summary>
+        /// <param name="nadador">Nadador cuyo turno se utilizará para contar.</param>
+        /// <returns>Cantidad de nadadores con el mismo turno.</returns>
         private int ContarNadadores(Nadador nadador)
         {
             int cantidad = 0;
@@ -87,6 +119,12 @@ namespace Entidades.Modelos
             return cantidad;
         }
 
+        /// <summary>
+        /// Valida si hay capacidad disponible en el natatorio para un nuevo nadador en el mismo turno.
+        /// </summary>
+        /// <param name="nadador">Nadador para el cual se verificará la capacidad.</param>
+        /// <returns>True si hay capacidad disponible, de lo contrario, se lanza una excepción NatatorioException.</returns>
+        /// <exception cref="NatatorioException">Se lanza cuando la capacidad está llena para el turno del nadador.</exception>
         public bool ValidarCapacidad(Nadador nadador)
         {
             if (ContarNadadores(nadador) < capacidad)
@@ -96,6 +134,14 @@ namespace Entidades.Modelos
             throw new NatatorioException($"Capacidad de {nadador.Deporte}, en el '{nadador.Turno}' llena.");
         }
 
+        /// <summary>
+        /// Sobrecarga del operador '+' que permite agregar un nadador al natatorio.
+        /// </summary>
+        /// <param name="natatorio">Natatorio al que se agregará el nadador.</param>
+        /// <param name="nadador">Nadador que se agregará al natatorio.</param>
+        /// <returns>El natatorio con el nuevo nadador agregado.</returns>
+        /// <exception cref="NatatorioException">Se lanza cuando el nadador ya está inscrito en el 
+        /// turno seleccionado o cuando la capacidad está llena.</exception>
         public static Natatorio operator +(Natatorio natatorio, Nadador nadador)
         {
             if (natatorio == nadador)
@@ -109,6 +155,12 @@ namespace Entidades.Modelos
             return natatorio;
         }
 
+        /// <summary>
+        /// Sobrecarga del operador '-' que permite quitar un nadador del natatorio.
+        /// </summary>
+        /// <param name="natatorio">Natatorio del que se quitará el nadador.</param>
+        /// <param name="nadador">Nadador que se quitará del natatorio.</param>
+        /// <returns>El natatorio con el nadador eliminado.</returns>
         public static Natatorio operator -(Natatorio natatorio, Nadador nadador)
         {
             int indice = natatorio[nadador];
@@ -116,11 +168,23 @@ namespace Entidades.Modelos
             return natatorio;
         }
 
+        /// <summary>
+        /// Sobrecarga del operador '==' que verifica si un nadador está inscrito en el natatorio.
+        /// </summary>
+        /// <param name="natatorio">Natatorio en el que se verificará la inscripción.</param>
+        /// <param name="nadador">Nadador cuya inscripción se verificará.</param>
+        /// <returns>True si el nadador está inscrito en el natatorio, de lo contrario, false.</returns>
         public static bool operator ==(Natatorio natatorio, Nadador nadador)
         {
             return natatorio.ListaNadadores.Contains(nadador);
         }
 
+        /// <summary>
+        /// Sobrecarga del operador '!=' que verifica si un nadador no está inscrito en el natatorio.
+        /// </summary>
+        /// <param name="natatorio">Natatorio en el que se verificará la inscripción.</param>
+        /// <param name="nadador">Nadador cuya inscripción se verificará.</param>
+        /// <returns>True si el nadador no está inscrito en el natatorio, de lo contrario, false.</returns>
         public static bool operator !=(Natatorio natatorio, Nadador nadador)
         {
             return !(natatorio == nadador);
